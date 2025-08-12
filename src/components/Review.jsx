638debaf513958547d3925b7163bd30d3a1c20be
Haanif <fataallamh258@gmail.com>
2025-08-12 20:43:48 +0700

    style : adding image to review user and the content

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,4 +1,9 @@
 import { Star } from "lucide-react";
+import Profile1 from "../assets/review/profile1.png";
+import Profile2 from "../assets/review/profile2.jpg";
+import Profile3 from "../assets/review/profile3.png";
+import Profile4 from "../assets/review/profile4.png";
+
 export default function Review() {
     return (
         <section className="font-primary py-10">
@@ -7,13 +12,13 @@ export default function Review() {
                 <h2 className="text-utama">Java Traveller</h2>
             </span>
 
-            <div className="flex justify-center gap-4 py-10 max-w-6xl mx-auto">
+            <div className="grid grid-cols-4 gap-4 py-10 px-20 mx-auto">
 
-                <div className="flex flex-col space-y-4 drop-shadow-xl border-1 border-gray-300 px-4 py-6 rounded-2xl">
+                <div className="flex flex-col space-y-4 drop-shadow-xl border-1 border-gray-300 px-4 py-6 rounded-2xl pb-24">
                     <div className="flex justify-start gap-6">
-                        <a className="rounded-full bg-gray-300 w-14 h-14"></a>
-                        <span className="text-md">
-                            <h1 className="font-senmibold text-black">Fariz</h1>
+                        <img src={Profile1} className="rounded-full bg-gray-300 w-14 h-14"></img>
+                        <span className="text-sm">
+                            <h1 className="font-medium text-black">Akira Hengki</h1>
                             <p className="font-medium text-utama">2024-04-14</p>
                             <span className="flex justify-center gap-1 mt-2 mb-2">
                                 <Star fill="#FFA600" stroke="#FFA600" size={20}/>
@@ -24,14 +29,13 @@ export default function Review() {
                             </span>
                         </span>
                     </div>
-                    <p className="text-justify text-sm">Terima kasih. Buat mas Aldi yg on time jemput dan semua agenda bisa berjalan dgn lancar. Dan menguasai jalur alternatif buat menghindari kemacetan. Dan Nagan Tour yg telah men support agenda tour sesuai dgn program. Next tour lagi bersama Nagan Tour.
-                    </p>
+                    <p className="text-justify text-sm">Di java bisa pesen tiket pesawat.. Paket tour dalam dan luar negeri.. Paket ziarah.. Pake eropa muslim dengan harga terjangkau... Mau sewa mobil atau bis juga bisa....</p>
                 </div>
                 <div className="flex flex-col space-y-4 drop-shadow-xl border-1 border-gray-300 px-4 py-6 rounded-2xl">
                     <div className="flex justify-start gap-6">
-                        <a className="rounded-full bg-gray-300 w-14 h-14"></a>
-                        <span className="text-md">
-                            <h1 className="font-senmibold text-black">Fariz</h1>
+                        <img src={Profile2} className="rounded-full bg-gray-300 w-14 h-14"></img>
+                        <span className="text-sm">
+                            <h1 className="font-medium text-black">Toni</h1>
                             <p className="font-medium text-utama">2024-04-14</p>
                             <span className="flex justify-center gap-1 mt-2 mb-2">
                                 <Star fill="#FFA600" stroke="#FFA600" size={20}/>
@@ -42,44 +46,41 @@ export default function Review() {
                             </span>
                         </span>
                     </div>
-                    <p className="text-justify text-sm">Terima kasih. Buat mas Aldi yg on time jemput dan semua agenda bisa berjalan dgn lancar. Dan menguasai jalur alternatif buat menghindari kemacetan. Dan Nagan Tour yg telah men support agenda tour sesuai dgn program. Next tour lagi bersama Nagan Tour.
-                    </p>
+                    <p className="text-justify text-sm">Terimakasih Java Traveller Yogyakarta. Atas keseruan liburan kami sekeluarga. Kami merasa puas. Mulai dari pemesanan bersama Mbak Fani, penjemputan di stasiun, wisata 3 hari dan pengantaran pulang ke stasiun bersama Mas Marcel yg selalu tepat</p>
                 </div>
                 <div className="flex flex-col space-y-4 drop-shadow-xl border-1 border-gray-300 px-4 py-6 rounded-2xl">
                     <div className="flex justify-start gap-6">
-                        <a className="rounded-full bg-gray-300 w-14 h-14"></a>
-                        <span className="text-md">
-                            <h1 className="font-senmibold text-black">Fariz</h1>
+                        <img src={Profile3} className="rounded-full bg-gray-300 w-14 h-14"></img>
+                        <span className="text-sm">
+                            <h1 className="font-medium text-black">Nanong Nury</h1>
                             <p className="font-medium text-utama">2024-04-14</p>
                             <span className="flex justify-center gap-1 mt-2 mb-2">
                                 <Star fill="#FFA600" stroke="#FFA600" size={20}/>
                                 <Star fill="#FFA600" stroke="#FFA600" size={20} />
                                 <Star fill="#FFA600" stroke="#FFA600" size={20} />
-                                <Star fill="#FFA600" stroke="#FFA600" size={20} />
-                                <Star fill="#FFA600" stroke="#FFA600" size={20} />
+                                <Star fill="white" stroke="gray" size={20} />
+                                <Star fill="white" stroke="gray" size={20} />
                             </span>
                         </span>
                     </div>
-                    <p className="text-justify text-sm">Terima kasih. Buat mas Aldi yg on time jemput dan semua agenda bisa berjalan dgn lancar. Dan menguasai jalur alternatif buat menghindari kemacetan. Dan Nagan Tour yg telah men support agenda tour sesuai dgn program. Next tour lagi bersama Nagan Tour.
-                    </p>
+                    <p className="text-justify text-sm">Di java bisa pesen tiket pesawat.. Paket tour dalam dan luar negeri.. Paket ziarah.. Pake eropa muslim dengan harga terjangkau</p>
                 </div>
                 <div className="flex flex-col space-y-4 drop-shadow-xl border-1 border-gray-300 px-4 py-6 rounded-2xl">
                     <div className="flex justify-start gap-6">
-                        <a className="rounded-full bg-gray-300 w-14 h-14"></a>
-                        <span className="text-md">
-                            <h1 className="font-senmibold text-black">Fariz</h1>
+                        <img src={Profile4} className="rounded-full bg-gray-300 w-14 h-14"></img>
+                        <span className="text-sm">
+                            <h1 className="font-medium text-black">Yuli</h1>
                             <p className="font-medium text-utama">2024-04-14</p>
                             <span className="flex justify-center gap-1 mt-2 mb-2">
                                 <Star fill="#FFA600" stroke="#FFA600" size={20}/>
                                 <Star fill="#FFA600" stroke="#FFA600" size={20} />
                                 <Star fill="#FFA600" stroke="#FFA600" size={20} />
                                 <Star fill="#FFA600" stroke="#FFA600" size={20} />
-                                <Star fill="#FFA600" stroke="#FFA600" size={20} />
+                                <Star fill="white" stroke="gray" size={20} />
                             </span>
                         </span>
                     </div>
-                    <p className="text-justify text-sm">Terima kasih. Buat mas Aldi yg on time jemput dan semua agenda bisa berjalan dgn lancar. Dan menguasai jalur alternatif buat menghindari kemacetan. Dan Nagan Tour yg telah men support agenda tour sesuai dgn program. Next tour lagi bersama Nagan Tour.
-                    </p>
+                    <p className="text-justify text-sm">Biro perjalanan wisata Jogja.. recommended kebutuhan wisata Indonesia</p>
                 </div>
                 
                 
